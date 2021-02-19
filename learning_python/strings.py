f6599086d5844c848b6dc00925e7f380fac92271
Caleb Cheptumo <ckibusia@gmail.com>
2021-02-19 01:00:12 -0800

    Add files via upload

---
+++ learning_python/strings.py
@@ -0,0 +1,10 @@
+Fname = 'caleb'
+Sname = 'Cheptumo'
+
+Fullname = Fname.capitalize() + ' ' + Sname 
+
+print(Fullname)
+print(len(Fname)) #length of characters
+
+#capitalize function to make first letter uppercase
+print(Fullname.endswith('Cheptumo'))
---
+++ learning_python/variables.py
@@ -0,0 +1,10 @@
+name = 'Caleb'  # sequence of characters - string
+age = 20  # integers
+pi = 3.14 # data type - float
+mask = ['n95' , 'glass' , 'local'] #list
+
+
+print(name)
+print(age)
+print(pi)
+print(mask)
